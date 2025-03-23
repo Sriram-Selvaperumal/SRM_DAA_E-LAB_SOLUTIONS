f04bf12eada02e147f4d234d6929d47461bdbb69
Sriram Selvaperumal <sriramselvaperumal@icloud.com>
2025-03-23 13:11:11 +0530

    Create the mask ate a block.cpp

---
+++ the mask ate a block.cpp
@@ -0,0 +1,28 @@
+/* Problem Descripton :
+The Mask ate a block of dynamite to save Tina during Dorian's rampage - now he has a taste for it.
+Help the Mask figure out how much dynamite he can eat without being destroyed. */
+
+// Solution:
+
+#include <bits/stdc++.h> 
+using namespace std; 
+int main() 
+ { 
+float a,c,d; 
+string b; 
+cin>>a>>b>>c; 
+float res; 
+int z=b.size(); 
+if(z==1) 
+d=b[0]-48; 
+else 
+d=(float)(b[0]-48)/(b[2]-48); 
+res=a*d*0.45*7.5; 
+if(res>c){ 
+cout<<res<<" the Mask should not eat it!"; 
+} 
+else 
+cout<<fixed<<setprecision(2)<<res<<" the Mask can eat it!"; 
+return 0; 
+cout<<"for"; 
+}
