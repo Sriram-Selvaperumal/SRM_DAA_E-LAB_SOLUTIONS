e96ea07081c2a2dc974065a8b73b8e31efc74bf6
Sriram Selvaperumal <sriramselvaperumal@icloud.com>
2025-03-29 20:21:37 +0530

    Create This is the easy version of the problem. The only difference is maximum value of Ai.cpp

---
+++ This is the easy version of the problem. The only difference is maximum value of Ai.cpp
@@ -0,0 +1,26 @@
+/* Question Description:
+This is the easy version of the problem. The only difference is maximum value of Ai.
+Once in Vettayapuram aranmanai Divan found an array A consisting of positive integers. Now he wants to reorder the elements of A to maximize the value of the following function: */
+
+//Solution:
+
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int const M = 5000000;
+int i, j, n, s, x, e[M + 100], f[M + 100], d[M + 100];
+
+signed main() {
+    cin>>n;
+    for (i = 1; i <= n; i++) scanf("%lld", &x), f[x]++;
+    for (i = 1; i <= M; i++)
+        for (j = i; j <= M; j += i)
+            e[i] += f[j];
+    for (i = M; i > 0; i--) {
+        for (s = 0, j = i * 2; j <= M; j += i)
+            s = max(s, d[j] - e[j] * i);
+        d[i] = e[i] * i + s;
+    }
+    printf("%lld\n", d[1]);
+    return 0;
+}
