8a307b4d41076108fb2f264efe8cb18ddfcf30da
Sriram Selvaperumal <sriramselvaperumal@icloud.com>
2025-04-08 14:04:32 +0530

    Create chef started watching a movle that runs for a total of XX minutes.cpp

---
+++ chef started watching a movle that runs for a total of XX minutes.cpp
@@ -0,0 +1,25 @@
+//Problem Statement:
+
+/* Chef started watching a movle that runs for a total of XX minutes.
+Chef has decided to watch the first YY minutes of the movle at twice the usual speed as he was warned by his friends that the movle gets Interesting only after the first YY minutes.
+How long will Chef spend watching the movle In total? */
+
+//Solution:
+
+#include <iostream> 
+using namespace std; 
+int main() { 
+        int x,y;
+        cin>>x>>y; 
+        cout<<(x-(y/2))<<endl; 
+ return 0; 
+    cout<<"while(t--)";
+}
+
+//Never forget to Star this Repo <3
+
+
+
+
+
+
